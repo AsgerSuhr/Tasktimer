ee4713adf24271b593ff1fa0ec401230d39fb6e6
AsgerSuhr <asger31@gmail.com>
2021-03-23 15:30:48 +0000

    test

---
+++ README.md
@@ -0,0 +1,19 @@
+# CP-app
+Small example of a secure electron app
+
+# Install
+please have node.js installed before doing the following steps
+https://nodejs.org/en/
+
+First download repository:
+```
+git clone https://github.com/AsgerSuhr/CP-app.git
+```
+Second Install node_modules:
+```
+npm install --save-dev electron
+```
+Third start the example app:
+```
+npm start
+```
---
+++ main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow () {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
+    // loading the window html
+    win.loadFile('index.html')
+}
+
+// when the content is loaded create the window
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
